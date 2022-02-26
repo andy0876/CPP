53e9c646485d2df7709c82750d4272f99b0409eb
Andy <59408071+CS-Andy@users.noreply.github.com>
2022-02-26 15:18:04 -0800

    Add files via upload

---
+++ tax.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double tax;
+    int total, subtract;
+
+    cout << "Enter tax percentage: ";
+    cin >> tax;
+}
