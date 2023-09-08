06f96335f991b20440b5ffcef2ba2cab8b2c176b
Andy Huynh <59408071+andy0876@users.noreply.github.com>
2023-09-07 23:58:22 -0700

    Add files via upload

---
+++ pointerreview04.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int boxes[6] = {5,4,3,2,1,0};
+    int jobs[4] = {42,88,99,51};
+    int* jam = nullptr;
+    int* cab = jobs;
+    jam = cab;
+    
+    cout << boxes[3] << " " << *boxes << " " << boxes[1] << endl;
+    
+    jam = &boxes[1];
+    cab++;
+    
+    cout << *(jam + 2) << " " << jam[3] << " " << *boxes << " "  << boxes[1] << endl;
+    
+    return 0;
+}
