08cc6a77f7c8e4579491d1ca6b2902e2dfafabbc
Andy Huynh <59408071+andy0876@users.noreply.github.com>
2023-09-09 12:24:37 -0700

    7.1.1 pointer.cpp

--- 7.1.1 pointer.cpp
+++ 7.1.1 pointer.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
 using namespace std;
+
 int main() {
   double account = 0;
   double* ptr = &account;
   *ptr = 1000;
-  cout << *ptr; // line 5: see below for error message
+  cout << *ptr;
   
   return 0;
 }
