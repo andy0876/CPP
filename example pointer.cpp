25330c41026f90d6ce1d94a0a3eb0b5a0b86927a
Andy Huynh <59408071+andy0876@users.noreply.github.com>
2023-09-09 12:37:16 -0700

    Update example pointer.cpp

--- example pointer.cpp
+++ example pointer.cpp
@@ -1,15 +1,28 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-int m = 10; // Assumed to be at address 20300
-int n = 20; // Assumed to be at address 20304
+int m = 10;
 int* p = &m;
 
-cout << &m << endl; // Prints 20300
-cout << &n << endl; // Does not print 20304
 cout << p << endl; // Prints 20300
+cout << *p << endl; // Prints 10
 
-  return 0;
-}
+int n = 20;
+cout << &n << endl; // Prints 20304
+
+p = &n;
+
+cout << p << endl; // Prints 20304
+cout << *p << endl; // Prints 20
+
+m = *p;
+
+cout << m << endl; // Prints 20
+
+// The following statements will cause errors:
+
+// m = p; // Error: incompatible types
+
+// &10; // Error: can't take address of literal
+
+// &p; // Error: can't take address of pointer variable
+
+// double x = 0;
+// p = &x; // Error: incompatible types
