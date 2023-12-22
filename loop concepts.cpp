0b3c368d7518321c22a2a1ea22101e79a60d9583
Andy Huynh <59408071+andy0876@users.noreply.github.com>
2023-12-22 00:39:52 -0800

    Add files via upload

---
+++ loop concepts.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    for ( int i = 0; i < 10; i++ )
+    {
+        cout << i << '\n';
+    }
+    
+    // int i = 0; // Don't forget to declare variables
+    
+    // while ( i < 10 ) // While i is less than 10
+    // {
+    //     cout << i << '\n';
+    //     i++; // Update i so the condition can be met eventually
+    // }
+}
